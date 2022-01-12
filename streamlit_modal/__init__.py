feb411996723ffbe179f3e419f1cf2788b0788da
Koen Vossen <info@koenvossen.nl>
2022-01-12 20:03:59 +0100

    Cleaup and make position of modal fixed

--- streamlit_modal/__init__.py
+++ streamlit_modal/__init__.py
@@ -19,60 +19,66 @@ def close():
 
 
 @contextmanager
-def container():
+def container(title=None, padding=100):
+    st.markdown(
+        """
+        <style>
+        div[data-modal-container='true'] {
+            position: fixed;
+        }
+
+        div[data-modal-container='true'] h1 a {
+            display: none
+        }
+
+        div[data-modal-container='true']::before {
+                position: fixed;
+                content: ' ';
+                left: 0;
+                right: 0;
+                top: 0;
+                bottom: 0;
+                z-index: 1000;
+                background-color: rgba(0, 0, 0, 0.5);
+        }
+
+        div[data-modal-container='true'] > div:first-child > div:first-child {
+            width: unset !important;
+            background-color: #f0f0f0;     
+            margin: auto;
+            padding: """ + str(padding) + """px;
+            margin-top: -""" + str(padding) + """px;
+            margin-left: -""" + str(padding) + """px;
+            margin-bottom: -""" + str(2 * padding) + """px;
+            z-index: 1001;
+            border-radius: 5px;
+        }
+        div[data-modal-container='true'] > div > div:nth-child(2)  {
+            z-index: 1003;
+            position: absolute;
+        }
+        div[data-modal-container='true'] > div > div:nth-child(2) > div {
+            text-align: right;
+        }
+        div[data-modal-container='true'] > div > div:nth-child(2) > div > button {
+            right: 0;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
     with st.container():
         _container = st.container()
+        if title:
+            _container.markdown(f"<h1>{title}</h1>", unsafe_allow_html=True)
+
+        close_ = st.button('X')
+        if close_:
+            close()
 
     components.html(
         """
-
         <script>
-
-        // MODAL
-
-        const styles = `
-                div[data-modal-container='true']::before {
-                        position: fixed;
-                        content: ' ';
-                        left: 0;
-                        right: 0;
-                        top: 0;
-                        bottom: 0;
-                        z-index: 1000;
-                        background-color: rgba(0, 0, 0, 0.5);
-                }
-
-                div[data-modal-container='true'] > div:first-child > div {
-                    width: unset !important;
-                    background-color: #f0f0f0;     
-                    margin: auto;
-                    padding: 100px;
-                    margin-top: -100px;
-                    margin-left: -100px;
-                    margin-bottom: -200px;
-                    z-index: 1001;
-                    border-radius: 5px;
-                }
-        `;
-
-
-        const exists = !!parent.document.querySelector("style[data-modal-css='true']");
-        if (!exists) {
-            var head = parent.document.head || parent.document.getElementsByTagName('head')[0],
-            styleTag = parent.document.createElement('style')
-        
-            styleTag.type = 'text/css'
-            styleTag.setAttribute("data-modal-css", "true");
-        
-            if (styleTag.styleSheet) {
-                styleTag.styleSheet.cssText = styles;
-            } else {
-                styleTag.appendChild(parent.document.createTextNode(styles))
-            }
-        
-            head.appendChild(styleTag)
-        }
-
         // MODAL
         const iframes = parent.document.body.getElementsByTagName('iframe');
         let container
