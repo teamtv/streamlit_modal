5e28eeb9fdfc14c9aa49cf3c93db35a1fafde033
WeifengLuo <wfluo@aibee.com>
2022-03-09 14:08:36 +0800

    support mutiple modals

--- streamlit_modal/__init__.py
+++ streamlit_modal/__init__.py
@@ -1,115 +1,150 @@
+from uuid import uuid4
 from contextlib import contextmanager
 
 import streamlit as st
 import streamlit.components.v1 as components
 
 
+class Modal:
+
+    def __init__(self, title, padding=100, max_width=None, background_color='#fff', key=None):
+        self.title = title
+        self.padding = padding
+        self.backgroud_color = background_color
+        self.max_width = max_width
+        self.key = key or f'streamlit-modal-{uuid4().hex}'
+
+    def is_open(self):
+        return st.session_state.get(f'{self.key}-opened', False)
+
+    def open(self):
+        st.session_state[f'{self.key}-opened'] = True
+        st.experimental_rerun()
+
+    def close(self):
+        st.session_state[f'{self.key}-opened'] = False
+        st.experimental_rerun()
+
+    @contextmanager
+    def container(self):
+        if self.max_width:
+            max_width = str(self.max_width) + "px"
+        else:
+            max_width = 'unset'
+
+        st.markdown(
+            f"""
+            <style>
+            div[data-modal-container='true'][key='{self.key}'] {{
+                position: fixed;
+                width: 100vw !important;
+                left: 0;
+                z-index: 1001;
+            }}
+
+            div[data-modal-container='true'][key='{self.key}'] > div:first-child {{
+                margin: auto;
+            }}
+
+            div[data-modal-container='true'][key='{self.key}'] h1 a {{
+                display: none
+            }}
+
+            div[data-modal-container='true'][key='{self.key}']::before {{
+                    position: fixed;
+                    content: ' ';
+                    left: 0;
+                    right: 0;
+                    top: 0;
+                    bottom: 0;
+                    z-index: 1000;
+                    background-color: rgba(0, 0, 0, 0.5);
+            }}
+            div[data-modal-container='true'][key='{self.key}'] > div:first-child {{
+                max-width: {max_width};
+            }}
+
+            div[data-modal-container='true'][key='{self.key}'] > div:first-child > div:first-child {{
+                width: unset !important;
+                background-color: {self.backgroud_color};     
+                padding: {self.padding}px;
+                margin-top: -{self.padding}px;
+                margin-left: -{self.padding}px;
+                margin-bottom: -{2*self.padding}px;
+                z-index: 1001;
+                border-radius: 5px;
+            }}
+            div[data-modal-container='true'][key='{self.key}'] > div > div:nth-child(2)  {{
+                z-index: 1003;
+                position: absolute;
+            }}
+            div[data-modal-container='true'][key='{self.key}'] > div > div:nth-child(2) > div {{
+                text-align: right;
+                padding-right: {self.padding}px;
+                max-width: {max_width};
+            }}
+
+            div[data-modal-container='true'][key='{self.key}'] > div > div:nth-child(2) > div > button {{
+                right: 0;
+            }}
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+        with st.container():
+            _container = st.container()
+            if self.title:
+                _container.markdown(
+                    f"<h1>{self.title}</h1>", unsafe_allow_html=True)
+
+            close_ = st.button('X', key=f'{self.key}-close')
+            if close_:
+                self.close()
+
+        components.html(
+            f"""
+            <script>
+            // STREAMLIT-MODAL-IFRAME-{self.key} <- Don't remove this comment. It's used to find our iframe
+            const iframes = parent.document.body.getElementsByTagName('iframe');
+            let container
+            for(const iframe of iframes)
+            {{
+            if (iframe.srcdoc.indexOf("STREAMLIT-MODAL-IFRAME-{self.key}") !== -1) {{
+                container = iframe.parentNode.previousSibling;
+                container.setAttribute('data-modal-container', 'true');
+                container.setAttribute('key', '{self.key}');
+            }}
+            }}
+            </script>
+            """,
+            height=0, width=0
+        )
+
+        with _container:
+            yield _container
+
+
+# keep compatible with old api
+
+_default_modal = Modal('', key='streamlit-modal-default')
+
+
 def is_open():
-    return st.session_state.get('modal_is_open', False)
+    return _default_modal.is_open()
 
 
-def open():
-    st.session_state.modal_is_open = True
-    st.experimental_rerun()
+def open():  # pylin: disable=redefined-builtin
+    return _default_modal.open()
 
 
 def close():
-    st.session_state.modal_is_open = False
-    st.experimental_rerun()
+    return _default_modal.close()
 
 
 @contextmanager
 def container(title=None, padding=100, max_width=None):
-    if max_width:
-        max_width = str(max_width) + "px";
-    else:
-        max_width = 'unset';
-
-    st.markdown(
-        """
-        <style>
-        div[data-modal-container='true'] {
-            position: fixed;
-            width: 100vw !important;
-            left: 0;
-            z-index: 1001;
-        }
-
-        div[data-modal-container='true'] > div:first-child {
-            margin: auto;
-        }
-
-        div[data-modal-container='true'] h1 a {
-            display: none
-        }
-
-        div[data-modal-container='true']::before {
-                position: fixed;
-                content: ' ';
-                left: 0;
-                right: 0;
-                top: 0;
-                bottom: 0;
-                z-index: 1000;
-                background-color: rgba(0, 0, 0, 0.5);
-        }
-        div[data-modal-container='true'] > div:first-child {
-            max-width: """ + max_width + """;
-        }
-
-        div[data-modal-container='true'] > div:first-child > div:first-child {
-            width: unset !important;
-            background-color: #f0f0f0;     
-            padding: """ + str(padding) + """px;
-            margin-top: -""" + str(padding) + """px;
-            margin-left: -""" + str(padding) + """px;
-            margin-bottom: -""" + str(2 * padding) + """px;
-            z-index: 1001;
-            border-radius: 5px;
-        }
-        div[data-modal-container='true'] > div > div:nth-child(2)  {
-            z-index: 1003;
-            position: absolute;
-        }
-        div[data-modal-container='true'] > div > div:nth-child(2) > div {
-            text-align: right;
-            padding-right: """ + str(padding) + """px;
-            max-width: """ + max_width + """;
-        }
-
-        div[data-modal-container='true'] > div > div:nth-child(2) > div > button {
-            right: 0;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-    with st.container():
-        _container = st.container()
-        if title:
-            _container.markdown(f"<h1>{title}</h1>", unsafe_allow_html=True)
-
-        close_ = st.button('X')
-        if close_:
-            close()
-
-    components.html(
-        """
-        <script>
-        // STREAMLIT-MODAL-IFRAME <- Don't remove this comment. It's used to find our iframe
-        const iframes = parent.document.body.getElementsByTagName('iframe');
-        let container
-        for(const iframe of iframes)
-        {
-          if (iframe.srcdoc.indexOf("STREAMLIT-MODAL-IFRAME") !== -1) {
-            container = iframe.parentNode.previousSibling;
-            container.setAttribute('data-modal-container', 'true');
-          }
-        }
-        </script>
-        """,
-        height=0, width=0
-    )
-
-    with _container:
-        yield _container
+    _default_modal.title = title
+    _default_modal.padding = padding
+    _default_modal.max_width = max_width
+    with _default_modal.container() as _container:
+        yield [_container]
